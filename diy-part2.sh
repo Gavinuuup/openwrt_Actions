6264c4c42d30d19d6965edc1e99344420f72f65f
Gavinuuup <62232251+Gavinuuup@users.noreply.github.com>
2023-06-29 16:53:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git luci-theme-argon package/lean
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.253/g' package/base-files/files/bin/config_generate
